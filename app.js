ca3e313196a71477c5114fb273b6e450647f2985
harrishandbook <harrishandbook@users.noreply.github.com>
2018-10-18 17:53:21 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+// Set constraints for the video stream
+var constraints = { video: { facingMode: "environment" }, audio: false };
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger")
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
+}
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
+
+
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
+
+
+
+
+//My axis orientation: invers image when y along is neg or when both x+y are positive
+    function calcAngle(x, y) {
+        if(x>0){    
+        return (180 / Math.PI) * Math.atan(y / x)+180;
+        }else{
+        return (180 / Math.PI) * Math.atan(y / x);
+            }
+    }
+    var axisInput = document.getElementByID("inputAxis");
+    var grid = document.getElementById("grid");
+    var axis = document.getElementById("axis");
+    var ax = 0;
+
+
+        /* Update input axis */
+axisInput.addEventListener("input", function(e) {
+    ax = axisInput.value;
+}, false);
+
+        //DeviceMOTION needed for gravity eval
+        window.addEventListener("devicemotion", function(event) {
+          // again, use vendor-prefixed transform property
+          grid.style.transform = 
+              "rotate(" + ( 90-calcAngle(event.accelerationIncludingGravity.x,event.accelerationIncludingGravity.y) ) + "deg) ";
+            
+          axis.style.transform = 
+              "rotate(" + ( ax + 180-calcAngle(event.accelerationIncludingGravity.x,event.accelerationIncludingGravity.y) ) + "deg) ";
+        });
+
